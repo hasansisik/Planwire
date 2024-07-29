9233dca4519112ad0b5a5ee4480fdc8ac8ad4bd4
hasansisik <gegify@gmail.com>
2024-07-29 22:09:59 +0300

    project

--- Web/app/navigator/profile/page.tsx
+++ Web/app/navigator/profile/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function ProfilePage() {
   return (
@@ -11,6 +13,66 @@ export default function ProfilePage() {
           </p>
         </div>
         <Button className="flex gap-2 text-xs">Kaydet</Button>
+      </div>
+      {/* Profile */}
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Kullanıcı Adı</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Kullanıcı adını düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="Kullanıcı Adı" className="w-[300px]" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">İş Pozisyonu</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            İş Pozisyonunu düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="İş Pozisyonu" className="w-[300px]" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Adres</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Adresinizi düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Textarea placeholder="Adres Bilgileri" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Şifre</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Şifrenizi düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input type="password" placeholder="Şifre" className="w-[300px]" />
+        <Input
+          type="password"
+          placeholder="Şifre Tekrar"
+          className="w-[300px]"
+        />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Telefon Numarası</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Telefon Numarasını düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="Telefon Numarası" className="w-[300px]" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Mail Adresi</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Mail Adresinizi düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="Mail Adresi" className="w-[300px]" />
       </div>
     </div>
   );
--- Web/app/navigator/profile/project-edit/page.tsx
+++ Web/app/navigator/profile/project-edit/page.tsx
@@ -1,4 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function ProjectPage() {
   return (
@@ -11,6 +22,54 @@ export default function ProjectPage() {
           </p>
         </div>
         <Button className="flex gap-2 text-xs">Kaydet</Button>
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Projeyi Seçin</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Projeyi düzenlemek için Seçin.
+          </p>
+        </div>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Lütfen Proje Seçin" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Projeler</SelectLabel>
+              <SelectItem value="santiye1">İstanbul Şantiye</SelectItem>
+              <SelectItem value="santiye2">Ankara Şantiye</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      {/* Project */}
+      <div className="grid grid-cols-3 gap-4 pt-20 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Proje Adı</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Proje adını düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="Proje Adı" className="w-[300px]" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Proje Kodu</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Proje Kodunu düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Input placeholder="Proje Kodu" className="w-[300px]" />
+      </div>
+      <div className="grid grid-cols-3 gap-4 py-5 border-b">
+        <div className="pb-5">
+          <p className="font-bold text-sm">Adres</p>
+          <p className="text-muted-foreground font-normal text-xs">
+            Adresinizi düzenleyebilir veya değiştirebilirsiniz.
+          </p>
+        </div>
+        <Textarea placeholder="Adres Bilgileri" />
       </div>
     </div>
   );
