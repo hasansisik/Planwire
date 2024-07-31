6d8b406964b4dd65003005888f6711991c5d3c3c
hasansisik <gegify@gmail.com>
2024-07-31 05:20:44 +0300

    fix

--- Web/app/(logged-out)/layout.tsx
+++ Web/app/(logged-out)/layout.tsx
@@ -1,20 +1,16 @@
-"use client";
-// app/layout.tsx
 import { LightDarkToggle } from "@/components/ui/light-dark-toggle";
-import { Provider } from "react-redux";
-import store from "../../redux/store";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function RootLayout({ children }: Props) {
+export default function LoggedOutLayout({ children }: Props) {
   return (
-    <Provider store={store}>
+    <>
       <div className="flex flex-col gap-5 min-h-screen items-center justify-center p-24">
         {children}
       </div>
       <LightDarkToggle className="fixed top-[calc(50%-12px)] right-2" />
-    </Provider>
+    </>
   );
 }
--- Web/app/layout.tsx
+++ Web/app/layout.tsx
@@ -1,17 +1,14 @@
-import type { Metadata } from "next";
+"use client";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 
-const poppins = Poppins({ 
-  subsets: ["latin"], 
-  weight: ["100","200","300","400", "500", "600", "700"]
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
-
-export const metadata: Metadata = {
-  title: "Planwire",
-  description: "Şantiye Yönetim Sistemi",
-};
 
 export default function RootLayout({
   children,
@@ -20,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(poppins.className,"light")}>{children}</body>
+      <body className={cn(poppins.className, "light")}>
+        <Provider store={store}>{children}</Provider>
+      </body>
     </html>
   );
 }
